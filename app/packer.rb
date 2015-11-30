8997c34fdf39bf387b7f5342a561bee67b0e38ec
Tana Tanoi <tanatanoi@gmail.com>
2015-11-30 15:01:24 +1300

    Created basic packet encoder -Only allows change of ID and adress as of yet -Class, flags, and counts all stay the same -Added tests to reflect these additions

--- app/packer.rb
+++ app/packer.rb
@@ -1,14 +1,41 @@
 #packer.rb
-
+require_relative 'header_structure'
 class Packer
-  def address_to_hex_buffer(address)
-    sections = address.split(".")
-    sections.map! do |label|
-      length = label.length.to_s(16)
-      hex_buffer = label.unpack('U*').collect{|x| x.to_s(16) }
-      hex_buffer.unshift(length)
-    end
-    sections.flatten
+  NULL_TERMINATOR = 0
+
+  def encode_header(id, address)
+    encode_id(id) + encode_flags + encode_counts + encode_address(address) + encode_type + encode_class
   end
 
+  def encode_flags
+    [0].pack("n")
+  end
+
+  def encode_counts
+    [1,0,0,0].pack("n*")
+  end
+
+  def encode_type
+    [1].pack("n")
+  end
+
+  def encode_class
+    [1].pack("n")
+  end
+
+  def encode_id(id)
+    [id].pack("n")
+  end
+
+  def encode_address(address)
+    labels = address.split(".")
+    array = []
+    labels.each do |label|
+      array.push(label.length)
+      array.push(label)
+    end
+    array.push(NULL_TERMINATOR)
+    packstring = array.map { |c| c.is_a?(Numeric) ? "c" : "a"+c.length.to_s }.join
+    array.pack(packstring)
+  end
 end
--- app/spec/packer_spec.rb
+++ app/spec/packer_spec.rb
@@ -2,13 +2,39 @@
 require_relative '../packer'
 RSpec.describe "packer" do
 
-  describe "#address_to_hex_buffer" do
-    address = "google.com"
-    subject { Packer.new.address_to_hex_buffer(address) }
+  # describe "#address_to_hex_buffer" do
+  #   address = "google.com"
+  #   subject { Packer.new.address_to_hex_buffer(address) }
+  #
+  #
+  #   context "when google.com is the address" do
+  #       it { is_expected.to eq "6 67 6f 67 6c 65 3 63 6f 6d".split }
+  #   end
+  # end
+  subject(:packer) { Packer.new }
 
+  describe "#encode_address" do
+    address = "powershop.co.nz"
+    result =  "\tpowershop\x02co\x02nz\x00".force_encoding('ASCII-8BIT')
+    subject { packer.encode_address(address) }
 
-    context "when google.com is the address" do
-        it { is_expected.to eq "6 67 6f 67 6c 65 3 63 6f 6d".split }
-    end
+    it { is_expected.to eq result }
+  end
+
+  describe "#encode_id" do
+    id = 1234
+    result = "\x04\xD2".force_encoding("ASCII-8BIT")
+    subject { packer.encode_id(id) }
+
+    it { is_expected.to eq result }
+  end
+
+  describe "#encode_header" do
+    id = 1234
+    address = "powershop.co.nz"
+    result = "\x04\xD2\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\tpowershop\x02co\x02nz\x00\x00\x01\x00\x01".force_encoding('ASCII-8BIT')
+    subject { packer.encode_header(id,address) }
+
+    it { is_expected.to eq result }
   end
 end
